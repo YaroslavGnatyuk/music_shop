c62d7c82e176b416c0efaac8aaf77ffb0e6ed512
YaroslavGnatyuk <yaroslavforjob@gmail.com>
2016-03-23 17:43:51 +0200

    some change

---
+++ src/main/java/Dao/CrudOperations.java
@@ -0,0 +1,35 @@
+package Dao;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+/**
+ * Created by yaroslav on 22.03.16.
+ */
+public abstract class CrudOperations<T>{
+    protected static SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+
+    public void createOrUpdate(T obj){
+        Session session = sessionFactory.openSession();
+        session.getTransaction().begin();
+
+        session.persist(obj);
+
+        session.getTransaction().commit();
+        session.close();
+    }
+
+    public abstract T findById(int id);
+
+    public void delete(T obj){
+        Session session = sessionFactory.openSession();
+        session.getTransaction().begin();
+
+        session.update(obj);
+        session.delete(obj);
+
+        session.getTransaction().commit();
+        session.close();
+    }
+}
---
+++ src/test/java/DaoTest.java
@@ -0,0 +1,5 @@
+/**
+ * Created by yaroslav on 21.03.16.
+ */
+public class DaoTest {
+}
