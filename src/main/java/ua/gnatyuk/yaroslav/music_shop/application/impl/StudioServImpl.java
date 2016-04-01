d650afc2c95c64e4d90ba1902240ebeeb0bf64d2
YaroslavGnatyuk <yaroslavforjob@gmail.com>
2016-04-01 15:22:59 +0300

    Added uml diagram

--- src/main/java/ua/gnatyuk/yaroslav/music_shop/application/impl/StudioServImpl.java
+++ src/main/java/ua/gnatyuk/yaroslav/music_shop/application/impl/StudioServImpl.java
@@ -1,20 +1,24 @@
 package ua.gnatyuk.yaroslav.music_shop.application.impl;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ua.gnatyuk.yaroslav.music_shop.application.StudioService;
 import ua.gnatyuk.yaroslav.music_shop.dao.DaoPersist;
 import ua.gnatyuk.yaroslav.music_shop.domain.musicrecord.Studio;
 
+import javax.inject.Inject;
+import javax.inject.Named;
+
 /**
  * Created by yaroslav on 31.03.16.
  */
 @Service
 public class StudioServImpl implements StudioService{
-    @Autowired
-    @Qualifier(value = "studioDAO")
+
+    /*@Autowired
+    @Qualifier(value = "studioDAO")*/
+    @Inject
+    @Named(value = "studioDAO")
     DaoPersist<Studio> daoStudio;
 
     @Transactional
